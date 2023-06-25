44dde4ca1a1fb1ff2e6bcdc042d6516b912fcb79
Дмитрий И. Сипунов <meteysh@gmail.com>
2023-06-25 14:36:07 +0300

    added random translate

--- trainer.js
+++ trainer.js
@@ -11,35 +11,28 @@ function translateWords() {
         var words = JSON.parse(jsonData);
     } else {
         errorElement.textContent = 'Загрузить файл-словарь';
-        // console.log('Загрузить файл-словарь');
     }
 
     var wordElement = document.getElementById("word");
     var translationElement = document.getElementById("translation");
-    var index = 0;
     var isRussianToEnglish = true;
 
     function showNextWord() {
+        var index = getRandomIndex(words); // Получить рандомный индекс
         var currentWord = words[index][isRussianToEnglish ? 0 : 1];
         var currentTranslation = words[index][isRussianToEnglish ? 1 : 0];
 
         wordElement.textContent = currentWord;
-        translationElement.textContent = ""; // Очистка перевода
+        translationElement.textContent = "";
 
         setTimeout(function () {
             translationElement.textContent = currentTranslation;
 
             setTimeout(function () {
-                index++;
-                if (index >= words.length) {
-                    index = 0;
-                    isRussianToEnglish = !isRussianToEnglish;
-                }
-
-                showNextWord(); // Показать следующее слово и перевод
+                showNextWord();
             }, 5000);
         }, 3000);
     }
 
-    showNextWord(); // Показать первое слово сразу же
+    showNextWord();
 }
